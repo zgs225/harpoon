b14f81fb5df69c077f3a35b23e88cdce9d36a3c3
翟根深 <zhaigenshen@youplus.cc>
2017-06-07 11:04:12 +0800

    Add build command

--- build.go
+++ build.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 // Build docker image
 
 func hBuild() {
 	c := loadConfig()
-	fmt.Println(*c)
+	fmt.Printf("[i] Building docker image %v\n", c.Image)
+	cmd := exec.Command("docker", "build", "-t", c.Image, ".")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("[i] Error.")
+	} else {
+		fmt.Println("[i] Done.")
+	}
 }
