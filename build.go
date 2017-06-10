7fc4b5351f3a05d7f5f9f291a9115bce5903fa0e
翟根深 <zhaigenshen@youplus.cc>
2017-06-10 16:43:36 +0800

    fix build command

--- build.go
+++ build.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -10,21 +9,14 @@ import (
 // Build docker image
 
 func hBuild(args []string) {
-	f := flag.NewFlagSet("harpoon build", flag.PanicOnError)
-	noCache := f.Bool("no-cache", false, "是否使用缓存")
-	erro := f.Parse(args)
-	if erro != nil {
-		panic(erro)
-	}
-
 	c := loadConfig()
 	c.check()
 	fmt.Printf("[i] Building docker image %v\n", c.Image)
+
 	cmdArgs := []string{"build", "-t", c.Image}
-	if *noCache {
-		cmdArgs = append(cmdArgs, "--no-cache")
-	}
+	cmdArgs = append(cmdArgs, args...)
 	cmdArgs = append(cmdArgs, ".")
+
 	cmd := exec.Command("docker", cmdArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
